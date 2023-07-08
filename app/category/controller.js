c31eda0e40427d4d11ff81407f5974e5681d7803
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-08 09:29:09 +0700

    create category page

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,9 +1,16 @@
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render('index', {
+      res.render('admin/category/view_category', {
         title: 'Category',
       });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render('admin/category/create');
     } catch (error) {
       console.log(error);
     }
--- app/category/router.js
+++ app/category/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { index } = require('./controller');
+const { index, viewCreate } = require('./controller');
 
 router.get('/', index);
+router.get('/create', viewCreate);
 
 module.exports = router;
