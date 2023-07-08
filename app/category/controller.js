a9e63883728e3bec38b6c11a7c660e9769baf9b9
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-08 16:28:32 +0700

    create categories

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,3 +1,5 @@
+const Category = require('./model');
+
 module.exports = {
   index: async (req, res) => {
     try {
@@ -11,6 +13,18 @@ module.exports = {
   viewCreate: async (req, res) => {
     try {
       res.render('admin/category/create');
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+      console.log(name);
+      // let category = await Category({name});
+      const category = await Category.create({ name });
+      // await category.save();
+      res.redirect('/category');
     } catch (error) {
       console.log(error);
     }
--- app/category/router.js
+++ app/category/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { index, viewCreate } = require('./controller');
+const { index, viewCreate, actionCreate } = require('./controller');
 
 router.get('/', index);
 router.get('/create', viewCreate);
+router.post('/create', actionCreate)
 
 module.exports = router;
