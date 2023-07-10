3e339fe211404bd61c6b2edd421d1aa9cffd32e9
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-10 09:55:05 +0700

    Notif and Messages Dinamis

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const categoryRouter = require('./app/category/router');
 const dashboardRouter = require('./app/dashboard/router');
@@ -14,11 +16,23 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+);
+
+app.use(flash());
+
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/adminlte', express.static(path.join(__dirname, 'node_modules/admin-lte')));
 
--- app/category/controller.js
+++ app/category/controller.js
@@ -3,32 +3,46 @@ const Category = require('./model');
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+
+      const alert = { message: alertMessage, status: alertStatus };
       const category = await Category.find();
+
+      console.log(alert);
+
       res.render('admin/category/view_category', {
-        title: 'Category',
         category: category,
+        alert: alert,
       });
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Error: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   viewCreate: async (req, res) => {
     try {
       res.render('admin/category/create');
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Error: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   actionCreate: async (req, res) => {
     try {
       const { name } = req.body;
-      console.log(name);
-      // let category = await Category({name});
       const category = await Category.create({ name });
-      // await category.save();
+
+      req.flash('alertMessage', 'Berhasil menambahkan data kategori');
+      req.flash('alertStatus', 'success');
+
       res.redirect('/category');
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Error: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   viewEdit: async (req, res) => {
@@ -39,7 +53,9 @@ module.exports = {
         category,
       });
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Error: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   actionEdit: async (req, res) => {
@@ -47,18 +63,31 @@ module.exports = {
       const { id } = req.params;
       const { name } = req.body;
       const category = await Category.findOneAndUpdate({ _id: id }, { name }, { new: true });
+
+      req.flash('alertMessage', 'Berhasil mengubah data kategori');
+      req.flash('alertStatus', 'success');
+
       res.redirect('/category');
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Gagal mengubah: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   actionDelete: async (req, res) => {
     try {
       const { id } = req.params;
       const category = await Category.findOneAndRemove({ _id: id });
+
+
+      req.flash('alertMessage', 'Berhasil menghapus data kategori');
+      req.flash('alertStatus', 'success');
+
       res.redirect('/category');
     } catch (error) {
-      console.log(error);
+      req.flash('alertMessage', `Gagal menghapus: ${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
 };
