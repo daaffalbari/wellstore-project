c7119810a41a5bf25e065aec2dedf4100037e699
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-22 01:10:37 +0700

    create edit profile api

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile } = require('./controller');
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+  dashboard,
+  profile,
+  editProfile,
+} = require('./controller');
 const { isLoginPlayer } = require('../middleware/auth');
+const multer = require('multer');
+const os = require('os');
 
 router.get('/landingPage', landingPage);
 router.get('/:id/detail', detailPage);
@@ -11,5 +23,6 @@ router.get('/history', isLoginPlayer, history);
 router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 router.get('/dashboard', isLoginPlayer, dashboard);
 router.get('/profile', isLoginPlayer, profile);
+router.put('/profile', isLoginPlayer, multer({ dest: os.tmpdir() }).single('image'), editProfile);
 
 module.exports = router;
