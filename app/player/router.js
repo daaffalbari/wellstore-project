37f4b79f3d6e1344869339f7660331ef487e8c87
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-21 23:23:51 +0700

    create api history and history detail

--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage, category, checkout } = require('./controller');
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require('./controller');
 const { isLoginPlayer } = require('../middleware/auth');
 
 router.get('/landingPage', landingPage);
 router.get('/:id/detail', detailPage);
 router.get('/category', category);
 router.post('/checkout', isLoginPlayer, checkout);
+router.get('/history', isLoginPlayer, history);
+router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 
 module.exports = router;
