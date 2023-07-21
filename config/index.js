a603eb98b17e7a3a06461f504b3cbe2368d1beea
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-07-21 22:04:51 +0700

    create api signin

--- config/index.js
+++ config/index.js
@@ -6,5 +6,6 @@ dotenv.config();
 module.exports = {
   rootPath: path.resolve(__dirname, '..'),
   serviceName: process.env.SERVICE_NAME,
-  urlDB: process.env.MONGO_URL,
+  jwtKey: process.env.SECRET,
+  urlDB: process.env.MONGO_URL, 
 };
